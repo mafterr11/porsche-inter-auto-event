7deeb038cebe7cc4d7be00794d9823ffe7900cc3
Alexandru Maftei <mafterr11@gmail.com>
2024-06-13 14:02:31 +0300

    .

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export function constructMetadata({
   title = "PIA | Offer Fest",
-  description = "",
+  description = "Doar în perioada 17-22 iunie, vizitați-ne la Porsche Pipera sau Porsche București Vest pentru a testa gama noastră largă de mașini. Vă așteptăm!",
   image = "/logo.png",
   icons = "/logo.png",
 }: {
