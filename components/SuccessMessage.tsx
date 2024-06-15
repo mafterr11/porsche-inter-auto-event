a7f0b2dc2adeb859450885a1cf9a199d66eb8c12
mafterr11 <mafterr11@gmail.com>
2024-06-15 16:42:45 +0300

    .

--- components/SuccessMessage.tsx
+++ components/SuccessMessage.tsx
@@ -14,13 +14,18 @@ const SuccessMessage = () => {
     <div className="flex h-full flex-col items-center justify-center">
       <Confetti
         active={showConfetti}
-        config={{ elementCount: 200, spread: 90 }}
+        config={{
+          elementCount: 300,
+          spread: 90,
+          startVelocity: 60,
+          colors: ["#002E7A", "#805239", "#78FAAE", "#000"],
+        }}
       />
       <h2 className="text-2xl font-semibold text-white">Succes!</h2>
       <p className="text-neutral-300">Ați transmis informațiile cu succes</p>
       <button
         onClick={handleRestart}
-        className="mt-4 rounded-md bg-[#384967] px-4 py-2 text-white hover:bg-[#384967]/80 f"
+        className="f mt-4 rounded-md bg-[#384967] px-4 py-2 text-white hover:bg-[#384967]/80"
       >
         Acasă
       </button>
