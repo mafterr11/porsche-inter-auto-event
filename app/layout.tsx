4e2f2042ebc927b94b2dc4421cf000b0385b74af
Alexandru Maftei <mafterr11@gmail.com>
2024-06-17 11:02:59 +0300

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,10 @@ export default function RootLayout({
       lang="en"
       className="font-futura relative overflow-x-hidden bg-[#384967] text-neutral-200 lg:max-xl:h-full"
     >
-      <body className="relative h-full w-full overflow-x-hidden">
+      <head>
         <GoogleAnalytics />
+      </head>
+      <body className="relative h-full w-full overflow-x-hidden">
         <GoogleCaptchaWrapper>{children}</GoogleCaptchaWrapper>
         <Footer />
         <Toaster />
