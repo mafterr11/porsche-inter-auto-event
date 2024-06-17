f2410d081b07dee7566602eca18fb98f541c9a64
Alexandru Maftei <mafterr11@gmail.com>
2024-06-17 11:03:59 +0300

    .

--- hooks/useGoogleAnalytics.ts
+++ hooks/useGoogleAnalytics.ts
@@ -4,7 +4,7 @@ import { usePathname } from 'next/navigation';
 import { useEffect } from 'react';
 import * as gtag from '../lib/gtag';
 
-export const useGoogleAnalytics = () => {
+export const useGoogleAnalytics = () => { 
   const pathname = usePathname();
 
   useEffect(() => {
