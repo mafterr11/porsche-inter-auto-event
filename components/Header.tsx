5a44f0c25c7b892c7cc226bdedc17f48cff1cec5
Alexandru Maftei <mafterr11@gmail.com>
2024-06-18 10:29:35 +0300

    .

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,17 +3,19 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="relative flex w-screen flex-col items-center justify-center gap-y-2">
-      <div className="relative h-24 w-24">
-        <Image
-          src="/logo.png"
-          fill
-          alt="logo"
-          priority
-          sizes="(max-width: 768px) 100vw, 24rem" // Add the sizes prop
-        />
+    <div className="relative w-screen ">
+      <div onClick={() =>  window.location.reload()} className="flex flex-col items-center justify-center gap-y-2 ">
+        <div  className="relative h-24 w-24 hover:cursor-pointer">
+          <Image
+            src="/logo.png"
+            fill
+            alt="logo"
+            priority
+            sizes="(max-width: 768px) 100vw, 24rem" // Add the sizes prop
+          />
+        </div>
+        <h1 className="p-5 text-3xl hover:cursor-pointer">Porsche Inter Auto</h1>
       </div>
-      <h1 className="text-3xl p-5">Porsche Inter Auto</h1>
     </div>
   );
 };
